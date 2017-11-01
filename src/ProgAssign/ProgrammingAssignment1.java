5aaae43919abf1c23fdece0754c17c071fa477f2
Lovika Gosain <32248818+lg579@users.noreply.github.com>
2017-11-01 15:01:20 -0400

    Began

---
+++ src/ProgAssign/ProgrammingAssignment1.java
@@ -0,0 +1,36 @@
+/*
+ *     1. Create a String saying "Wow, the Dodgers are in the World Series!"
+ *        1. Using subString
+ *           1. Print out just World Series.
+ *           2. Print out "the Dodgers".
+ *           3. Can you print above two names using a different way?
+ *          
+ *     2. Create a variable containing the year the Dodgers last won a world series
+ *         Hint: It's 1988.
+ *     3. Figure out how many years it's been since they won. Store that in a variable.
+ *     4. Create a statement saying: "It's been this many years since the Dodgers won a World Series: <<Insert that value here>>.
+
+ *   I've attached a file you can use as a starting point.
+ *   
+ *
+*/
+
+package ProgAssign;
+
+ 
+
+public class ProgrammingAssignment1 {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Create a String saying "Wow, the Dodgers are in the World Series!"
+		//Write your code here....
+		 String string1 = "Wow, the Dodgers are in the World Series!";
+		 String part = string1.substring(28,40);
+         System.out.println(part);
+		
+		
+	}
+
+}
